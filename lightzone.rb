413daad44ee7b0f53ea5344de88341678a55373c
ktgw0316 <ktgw0316@users.noreply.github.com>
2018-12-22 13:30:57 +0900

    Update lightzone.rb

--- lightzone.rb
+++ lightzone.rb
@@ -1,15 +1,15 @@
 class Lightzone < Formula
   desc "Open-source professional-level digital darkroom software"
   homepage "http://lightzoneproject.org/"
-  url "https://github.com/ktgw0316/LightZone/archive/4.1.8.tar.gz"
-  sha256 "c92666ebcb4d066a5acbe3c41b6c4d74f78f9ac68998e3ac8ac3e13bb316905e"
+  url "https://github.com/ktgw0316/LightZone/archive/4.1.9.tar.gz"
+  sha256 "8fa2dee0edbb55c9b2375aa1e76f55ef7bf68aae1e0bc5f1db58f99a67359f12"
   head "https://github.com/ktgw0316/LightZone.git"
 
-  depends_on :java => "1.6+"
+  depends_on :java => "1.8"
   depends_on "ant" => :build
   depends_on "pkg-config" => :build
   depends_on "git" => :build
-  depends_on "llvm" => :build
+  depends_on "libomp" => :build
   depends_on "rsync" => :build
   depends_on "autoconf" => :build
   depends_on "libtiff"
@@ -19,7 +19,7 @@ class Lightzone < Formula
 
   def install
     ENV.deparallelize
-    system "ant", "-f", "macosx/build.xml", "build-bundle"
+    system "JAVA_HOME=$(/usr/libexec/java_home -v 1.8)" "ant", "-f", "macosx/build.xml", "build-bundle"
     prefix.install "macosx/release/LightZone.app"
     bin.install_symlink prefix/"LightZone.app/Contents/bin/lightzone"
   end
