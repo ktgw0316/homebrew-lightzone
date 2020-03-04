732f0dd34855053b0aae03cab454e3a09515c77e
Masahiro Kitagawa <arctica0316@gmail.com>
2020-03-05 00:05:37 +0900

    Update lightzone.rb for v4.2.0

--- lightzone.rb
+++ lightzone.rb
@@ -1,17 +1,18 @@
 class Lightzone < Formula
   desc "Open-source professional-level digital darkroom software"
   homepage "http://lightzoneproject.org/"
-  url "https://github.com/ktgw0316/LightZone/archive/4.1.9.tar.gz"
-  sha256 "b8e022bc565acdd91ca617cf38098109c9a33febc45641aaf55be7458761a21a"
+  url "https://github.com/ktgw0316/LightZone/archive/4.2.0.tar.gz"
+  sha256 "2d9cccb0a41fa3657fbef025a34fa6836c3b857e9f960e0526704438fa6503c7"
   head "https://github.com/ktgw0316/LightZone.git"
 
-  depends_on :java => "1.8"
+  depends_on :java => "11"
   depends_on "ant" => :build
   depends_on "pkg-config" => :build
   depends_on "git" => :build
   depends_on "libomp" => :build
   depends_on "rsync" => :build
   depends_on "autoconf" => :build
+  depends_on "lensfun"
   depends_on "libtiff"
   depends_on "libjpeg-turbo"
   depends_on "libxml2"
