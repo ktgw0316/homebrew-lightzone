c71e5a5efebabf171c5c521a5b27f6193da7c92a
KITAGAWA Masahiro <arctica0316@gmail.com>
2020-05-03 23:02:26 +0900

    Update for v4.2.1

--- lightzone.rb
+++ lightzone.rb
@@ -1,8 +1,8 @@
 class Lightzone < Formula
   desc "Open-source professional-level digital darkroom software"
   homepage "http://lightzoneproject.org/"
-  url "https://github.com/ktgw0316/LightZone/archive/4.2.0.tar.gz"
-  sha256 "2d9cccb0a41fa3657fbef025a34fa6836c3b857e9f960e0526704438fa6503c7"
+  url "https://github.com/ktgw0316/LightZone/archive/4.2.1.tar.gz"
+  sha256 "00319c1c41f4929484af3147985960d7aafe352fbd4477de9fe8edf210ad5df7"
   head "https://github.com/ktgw0316/LightZone.git"
 
   depends_on :java => "11"
